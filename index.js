702d6247cab1e6a877527db36db1cd6acdc1088f
SamHarron <harronsam@gmail.com>
2021-08-12 22:51:46 -0400

    commit tree 05de6441d357875d19431cd79c32d8c4930b5970

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const test = "Sam"
+
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement('h1')
+    newHeader.id = 'victory'
+    newHeader.innerHTML = "Sam is the champion!";
